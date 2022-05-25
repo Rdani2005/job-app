8759ac3de4c4e8f3b688a4d56a279801fca03c17
Rdani2005 <dricardosc15@gmail.com>
2022-05-24 19:41:28 -0600

    some features

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -17,12 +17,26 @@ const Home = () => {
         navigate("/home")
     }
 
-        return (
+    return (
 
-            <main className="min-h-screen">
-                <Slider />
-            </main>
-        )
+        <main className="min-h-screen">
+            <Slider />
+            <section className="container mt-20 text-white">
+                <div className="grid grid-cols-1 md:grid-cols-2 bg-cristal h-96">
+                    <div className="text-center flex justify-center items-center">
+                        <h1 className="text-5xl">
+                            Quienes somos?
+                        </h1>
+                    </div>
+                    <div className="text-center flex justify-center items-center">
+                        <p className="text-lg">
+                            Somos una empresa destinada a ayudar a personas a buscar comodidad con empresas reconocidas a nivel mundial
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
 }
 
 // const Titulo = styled.p`
